f2be512d9bd5924e91b0468f6ca01395913bf008
Tobias <tschnuerpel@gmail.com>
2019-02-08 13:15:57 +0100

    Order Model angepasst

--- api/models/order.js
+++ api/models/order.js
@@ -2,18 +2,6 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    firstname: {
-        type: String,
-        trim: true,
-        minlength: 2,
-        maxlength: 30
-    },
-    lastname: {
-        type: String,
-        trim: true,
-        minlength: 2,
-        maxlength: 30
-    },
     comment: {
         type: String,
         minlength: 2,
@@ -22,7 +10,12 @@ const orderSchema = mongoose.Schema({
     amount: {
         type: Number,
         min: 1,
-        max: 9999
+        max: 9999,
+        required: true
+    },
+    date : {
+        type: Date,
+        require: true
     } 
 }); 
 
